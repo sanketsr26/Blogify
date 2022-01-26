7826ad13d1c3443db61f6e4a47eb6a7f7725e8e3
Rapkal, Sanket <Sanket.Rapkal@cdk.com>
2022-01-26 22:26:59 +0530

    adding react, dom libraries

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react"
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to Blogify</h1>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"))
