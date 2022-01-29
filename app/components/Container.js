9e5930e1d6b9b85f45e17dc5fd9417ff03ba0834
Sanket_sr <sanketrapkal47@gmail.com>
2022-01-30 02:01:54 +0530

    Container wrapper

---
+++ app/components/Container.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Container = props => {
+  return (
+    <div
+      className={
+        "container " + (props.wide ? "" : "container--narrow") + " py-md-5"
+      }
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export default Container
