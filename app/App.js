630acd2fe87e8fc2118d189dd46748eb44a7bc06
Sanket_sr <sanketrapkal47@gmail.com>
2022-01-30 00:14:22 +0530

    setting up static Home page & routes

--- app/App.js
+++ app/App.js
@@ -1,11 +1,23 @@
 import React from "react"
 import ReactDom from "react-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Terms from "./components/Terms"
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome To Blogify!</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
