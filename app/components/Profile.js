587bae5c5f3ccc5d13e3a39c65cbca693e60bf96
Rapkal, Sanket <sanketrapkal47@gmail.com>
2022-03-06 15:05:01 +0530

    follow user feature

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react"
 import { useParams } from "react-router-dom"
 import Axios from "axios"
+import { useImmer } from "use-immer"
 import Page from "./Page"
 import StateContext from "../context/StateContext"
 import ProfilePosts from "./ProfilePosts"
@@ -10,14 +11,20 @@ const Profile = () => {
 
   const appState = useContext(StateContext)
 
-  const [profileData, setProfileData] = useState({
-    profileUsername: "...",
-    profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
-    counts: { postCount: "", followerCount: "", followingCount: "" }
+  const [state, setState] = useImmer({
+    followActionLoading: false,
+    startFollowingRequestCount: 0,
+    stopFollowingRequestCount: 0,
+    profileData: {
+      profileUsername: "...",
+      profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+      counts: { postCount: "", followerCount: "", followingCount: "" }
+    }
   })
 
   useEffect(() => {
     const request = Axios.CancelToken.source()
+    console.log(username)
     const fetchProfileData = async () => {
       try {
         const response = await Axios.post(
@@ -27,7 +34,9 @@ const Profile = () => {
           },
           { cancelToken: request.token }
         )
-        setProfileData(response.data)
+        setState(draft => {
+          draft.profileData = response.data
+        })
       } catch (error) {
         console.log(error)
       }
@@ -36,28 +45,120 @@ const Profile = () => {
     return () => {
       request.cancel()
     }
-  }, [])
+  }, [username])
+
+  useEffect(() => {
+    if (state.startFollowingRequestCount) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+      const request = Axios.CancelToken.source()
+      const fetchProfileData = async () => {
+        try {
+          const response = await Axios.post(
+            `/addFollow/${state.profileData.profileUsername}`,
+            { token: appState.user.token },
+            { cancelToken: request.token }
+          )
+          setState(draft => {
+            draft.profileData.isFollowing = true
+            draft.profileData.counts.followerCount++
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      fetchProfileData()
+      return () => {
+        request.cancel()
+      }
+    }
+  }, [state.startFollowingRequestCount])
+
+  useEffect(() => {
+    if (state.stopFollowingRequestCount) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+      const request = Axios.CancelToken.source()
+      const fetchProfileData = async () => {
+        try {
+          const response = await Axios.post(
+            `/removeFollow/${state.profileData.profileUsername}`,
+            { token: appState.user.token },
+            { cancelToken: request.token }
+          )
+          setState(draft => {
+            draft.profileData.isFollowing = false
+            draft.profileData.counts.followerCount--
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      fetchProfileData()
+      return () => {
+        request.cancel()
+      }
+    }
+  }, [state.stopFollowingRequestCount])
+
+  const handleFollowingAction = () => {
+    setState(draft => {
+      draft.startFollowingRequestCount++
+    })
+  }
+
+  const handleUnfollowingAction = () => {
+    setState(draft => {
+      draft.stopFollowingRequestCount++
+    })
+  }
+  console.log(state)
 
   return (
     <>
       <Page title="Profile">
         <h2>
-          <img className="avatar-small" src={profileData.profileAvatar} />{" "}
-          {profileData.profileUsername}
-          <button className="btn btn-primary btn-sm ml-2">
-            Follow <i className="fas fa-user-plus"></i>
-          </button>
+          <img className="avatar-small" src={state.profileData.profileAvatar} />{" "}
+          {state.profileData.profileUsername}
+          {appState.loggedIn &&
+            !state.profileData.isFollowing &&
+            appState.user.username != state.profileData.profileUsername &&
+            state.profileData.profileUsername != "..." && (
+              <button
+                className="btn btn-primary btn-sm ml-2"
+                onClick={handleFollowingAction}
+                disabled={state.followActionLoading}
+              >
+                Follow <i className="fas fa-user-plus"></i>
+              </button>
+            )}
+          {appState.loggedIn &&
+            state.profileData.isFollowing &&
+            appState.user.username != state.profileData.profileUsername &&
+            state.profileData.profileUsername != "..." && (
+              <button
+                className="btn btn-danger btn-sm ml-2"
+                onClick={handleUnfollowingAction}
+                disabled={state.followActionLoading}
+              >
+                Unfollow <i className="fas fa-user-times"></i>
+              </button>
+            )}
         </h2>
 
         <div className="profile-nav nav nav-tabs pt-2 mb-4">
           <a href="#" className="active nav-item nav-link">
-            Posts: {profileData.counts.postCount}
+            Posts: {state.profileData.counts.postCount}
           </a>
           <a href="#" className="nav-item nav-link">
-            Followers: {profileData.counts.followerCount}
+            Followers: {state.profileData.counts.followerCount}
           </a>
           <a href="#" className="nav-item nav-link">
-            Following: {profileData.counts.followingCount}
+            Following: {state.profileData.counts.followingCount}
           </a>
         </div>
         <ProfilePosts />
--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -28,7 +28,7 @@ const ProfilePosts = () => {
     return () => {
       request.cancel()
     }
-  }, [])
+  }, [username])
 
   if (isLoading) {
     return <LoaderIcon />
@@ -38,14 +38,17 @@ const ProfilePosts = () => {
     <div className="list-group">
       {posts.map((post, key) => {
         const date = new Date(post.createdDate)
-        const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+        const formattedDate = `${
+          date.getMonth() + 1
+        }/${date.getDate()}/${date.getFullYear()}`
         return (
           <Link
             key={post._id}
             to={`/post/${post._id}`}
             className="list-group-item list-group-item-action"
           >
-            <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong>{" "}
+            <img className="avatar-tiny" src={post.author.avatar} />{" "}
+            <strong>{post.title}</strong>{" "}
             <span className="text-muted small">on {formattedDate} </span>
           </Link>
         )
